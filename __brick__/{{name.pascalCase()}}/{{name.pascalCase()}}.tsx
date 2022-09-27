31c8919dbca2c5551b95335c933d8bffedf5fa9a
Eduardo <rcchristiane@gmail.com>
2022-09-27 10:34:01 -0500

    comit inicial

---
+++ __brick__/{{name.pascalCase()}}/{{name.pascalCase()}}.tsx
@@ -0,0 +1,19 @@
+/**
+ * Componente generada automáticamente con mason-CLI
+ */
+
+import styles from './{{name.pascalCase()}}.module.css';
+
+
+const {{ name.pascalCase() }} = () => {
+
+
+    return (
+        <>
+            <h1>{{ name.pascalCase() }} Component</h1>
+            <h2>I'm alive!</h2>
+        </>
+    );
+}
+
+export default {{ name.pascalCase() }};
